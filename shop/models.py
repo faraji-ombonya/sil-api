111898f036e3621e5fcea23a1cae46178a4b30d9
Faraji Ombonya <farajiombonya@gmail.com>
2025-01-31 22:06:48 +0300

    add an order item model

--- shop/models.py
+++ shop/models.py
@@ -2,17 +2,30 @@ import uuid
 
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
 
 
-class Category(models.Model):
+class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
 
+    # slugify the name
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
-class Product(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class Product(BaseModel):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
@@ -20,14 +33,18 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
-class Customer(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Customer(BaseModel):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
 
 
-class Order(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Order(BaseModel):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+
+class OrderItem(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    
