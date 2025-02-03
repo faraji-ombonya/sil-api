d487f9ef478d2260b33e1aa16bc3dea0f583ba51
Faraji Ombonya <farajiombonya@gmail.com>
2025-02-03 11:32:36 +0300

    setup PostgreSQL database

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -93,9 +93,17 @@ WSGI_APPLICATION = "PROJECT.wsgi.application"
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    },
+    "sqlite": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
 }
 
 
@@ -252,3 +260,4 @@ TEMPLATES = [
         },
     },
 ]
+
