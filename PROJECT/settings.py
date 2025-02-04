e7c3fdc54374bfffba7309d2e0e20c85dff23c87
Faraji Ombonya <farajiombonya@gmail.com>
2025-02-04 18:15:14 +0300

    use sqlite to run tests on CI pipeline

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -108,8 +108,23 @@ DATABASES = {
 
 # Use sqlite in CI
 if os.getenv("CI") == "True":
-    DATABASES["default"]["ENGINE"] = "django.db.backends.sqlite3"
-    DATABASES["default"]["NAME"] = BASE_DIR / "db.sqlite3"
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
